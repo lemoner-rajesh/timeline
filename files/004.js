6120c3a636b274b42d580d2a1e693cf1c0168c55
lemoner-rajesh <32484035+lemoner-rajesh@users.noreply.github.com>
2026-05-03 07:09:02 +0000

    asdf

---
+++ files/004.js
@@ -0,0 +1,22 @@
+CDRonJSONPLoad({
+  "categories": [
+    { "id": "1", "title": "Art & Culture", "color": "#1f77b4" },
+    { "id": "2", "title": "Music", "color": "#ff7f0e" },
+    { "id": "3", "title": "Science", "color": "#2ca02c" },
+    { "id": "4", "title": "Philosophy", "color": "#d62728" }
+  ],
+  "events": [
+    {
+      "id": 1,
+      "title": "Test Event 1",
+      "text": "Sample text",
+      "fullText": "Sample full text",
+      "categoryId": "1",
+      "dateFormat": "auto",
+      "externalLink": "",
+      "media": [],
+      "startDate": "2020-01-01 00:00:00",
+      "endDate": "2020-01-01 00:00:00"
+    }
+  ]
+})
