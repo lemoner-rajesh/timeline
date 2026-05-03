92665050c5f32fc206c01290f4c198c83b05475a
lemoner-rajesh <32484035+lemoner-rajesh@users.noreply.github.com>
2026-05-03 12:03:02 +0000

    tasdf

---
+++ files/02.js
@@ -0,0 +1,52 @@
+CDRonJSONPLoad([
+  {
+    "id": 1,
+    "title": "Falling down to Earth",
+    "text": "Don't miss this rare chance to hear world-famous Laura Thompson share the secrets of her success...",
+    "fullText": "### **Event highlights**\n\n* Interactive exhibits\n* Photographs and videos...",
+    "categoryId": "17654",
+    "dateFormat": "auto",
+    "externalLink": "",
+    "media": [
+      {
+        "id": 2,
+        "src": "https://www.chronoflotimeline.com/assets/images/home-events/image31.jpg",
+        "caption": "",
+        "type": "Image",
+        "imagePosition": "0,0",
+        "externalMediaThumb": "",
+        "externalMediaType": "",
+        "externalMediaId": "",
+        "orderIndex": 5,
+        "eventId": 1
+      }
+    ],
+    "startDate": "2018-01-01 09:00:00",
+    "endDate": "2018-01-01 17:00:00"
+  },
+  {
+    "id": 3,
+    "title": "Married to Work",
+    "text": "Join us in celebrating the work of legendary artist Roberto Bertrand...",
+    "fullText": "### **Event highlights**\n\n* Interactive exhibits\n* Photographs and videos...",
+    "categoryId": "17653",
+    "dateFormat": "auto",
+    "externalLink": "",
+    "media": [
+      {
+        "id": 4,
+        "src": "https://www.chronoflotimeline.com/assets/images/home-events/image30.jpg",
+        "caption": "",
+        "type": "Image",
+        "imagePosition": "0,0",
+        "externalMediaThumb": "",
+        "externalMediaType": "",
+        "externalMediaId": "",
+        "orderIndex": 5,
+        "eventId": 3
+      }
+    ],
+    "startDate": "2018-01-07 09:00:00",
+    "endDate": "2018-01-07 17:00:00"
+  }
+])
