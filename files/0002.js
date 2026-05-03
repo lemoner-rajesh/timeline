ed85d8d653e26a64cc4d48678b6119daa0e65a46
lemoner-rajesh <32484035+lemoner-rajesh@users.noreply.github.com>
2026-05-03 11:47:47 +0000

    test

---
+++ files/0002.js
@@ -0,0 +1,54 @@
+CDRonJSONPLoad(
+	[
+      {  
+         "id":1,
+         "title":"Falling down to Earth",
+         "text":"Don't miss this rare chance to hear world-famous Laura Thompson share the secrets of her success. The reclusive philosopher does not make many public appearances nowadays, preferring to spend time with her family, but has agreed to return to the public spotlight one more time after being awarded our Lifetime Achievement award.",
+         "fullText":"### **Event highlights**\n\n* Interactive exhibits\n* Photographs and videos\n* Childrens' activities\n   * Find the secret treasures hidden around the building\n   * Can you escape our fiendish maze\n   * Get a free selfie with a famous person\n\n### **Opening times**\n\nThis exhibition is open from 9am to 5pm.\n\n### **Ticket prices**\n\n* Adults: Â£18.50\n* Children: Â£10.00\n\n### **About us**\nWe're the Museum of Made-Up Stuff. We don't really exist. But if we did, we'd be amazing!\n\n### **Address**\nThe Museum of Made-Up Stuff,\n147 Never Never Avenue,\nAvalon,\nDream Land\n**Tel:** 012345 678910\n\n### **How to find us**\n\nWe're easy to find. Just call up Worzel the Flying Whale. He'll fly you straight to our front door. Alternatively, you can find directtions on this [Google Map](https://www.google.co.uk/maps/place/London/@51.5005311,-0.1328713,11z/data=!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5073509!4d-0.1277583?hl=en).\n",
+         "categoryId":"17654",
+         "dateFormat":"auto",
+         "externalLink":"",
+         "media":[  
+            {  
+               "id":2,
+               "src":"https://www.chronoflotimeline.com/assets/images/home-events/image31.jpg",
+               "caption":"",
+               "type":"Image",
+               "imagePosition":"0,0",
+               "externalMediaThumb":"",
+               "externalMediaType":"",
+               "externalMediaId":"",
+               "orderIndex":5,
+               "eventId":1
+            }
+         ],
+         "startDate":"2018-01-01 09:00:00",
+         "endDate":"2018-01-01 17:00:00",
+      },
+      {  
+         "id":3,
+         "title":"Married to Work",
+         "text":"Join us in celebrating the work of legendary artist Roberto Bertrand. This exhibition will include rarely seen footage and photographs, helping to shed light on what made Roberto Bertrand so special. We also take a look at the people who provided inspiration to Roberto Bertrand in his journey from obscurity to becoming one of the leading figures in his field.",
+         "fullText":"### **Event highlights**\n\n* Interactive exhibits\n* Photographs and videos\n* Childrens' activities\n   * Find the secret treasures hidden around the building\n   * Can you escape our fiendish maze\n   * Get a free selfie with a famous person\n\n### **Opening times**\n\nThis exhibition is open from 9am to 5pm.\n\n### **Ticket prices**\n\n* Adults: Â£18.50\n* Children: Â£10.00\n\n### **About us**\nWe're the Museum of Made-Up Stuff. We don't really exist. But if we did, we'd be amazing!\n\n### **Address**\nThe Museum of Made-Up Stuff,\n147 Never Never Avenue,\nAvalon,\nDream Land\n**Tel:** 012345 678910\n\n### **How to find us**\n\nWe're easy to find. Just call up Worzel the Flying Whale. He'll fly you straight to our front door. Alternatively, you can find directtions on this [Google Map](https://www.google.co.uk/maps/place/London/@51.5005311,-0.1328713,11z/data=!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5073509!4d-0.1277583?hl=en).\n",
+         "categoryId":"17653",
+         "dateFormat":"auto",
+         "externalLink":"",
+         "media":[  
+            {  
+               "id":4,
+               "src":"https://www.chronoflotimeline.com/assets/images/home-events/image30.jpg",
+               "caption":"",
+               "type":"Image",
+               "imagePosition":"0,0",
+               "externalMediaThumb":"",
+               "externalMediaType":"",
+               "externalMediaId":"",
+               "orderIndex":5,
+               "eventId":3
+            }
+         ],
+         "startDate":"2018-01-07 09:00:00",
+         "endDate":"2018-01-07 17:00:00",
+      } 
+	]
+)
