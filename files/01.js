da4e0cac7fd476db8ea3f04b88493b415917d989
lemoner-rajesh <32484035+lemoner-rajesh@users.noreply.github.com>
2026-05-03 11:51:54 +0000

    test

---
+++ files/01.js
@@ -0,0 +1,34 @@
+CDRonJSONPLoad({
+  "categories": [
+    {
+      "id": "cat_1",
+      "name": "Personal",
+      "accentColor": "#ff0000",
+      "icon": "star"
+    },
+    {
+      "id": "cat_2",
+      "name": "Work",
+      "accentColor": "#00ff00",
+      "icon": "briefcase"
+    }
+  ],
+  "events": [
+    {
+      "id": 1,
+      "title": "Test Event 1",
+      "text": "Mapping to Personal category",
+      "categoryId": "cat_1",
+      "startDate": "2024-01-01 09:00:00",
+      "endDate": "2024-01-01 10:00:00"
+    },
+    {
+      "id": 2,
+      "title": "Test Event 2",
+      "text": "Mapping to Work category",
+      "categoryId": "cat_2",
+      "startDate": "2024-01-02 09:00:00",
+      "endDate": "2024-01-02 10:00:00"
+    }
+  ]
+})
