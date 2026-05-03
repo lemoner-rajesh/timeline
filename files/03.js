9e3830abec7a46cf8573c8696836269f61302271
lemoner-rajesh <32484035+lemoner-rajesh@users.noreply.github.com>
2026-05-03 12:58:57 +0000

    adsf

---
+++ files/03.js
@@ -0,0 +1,34 @@
+CDRonJSONPLoad(
+	[
+      {  
+         "id": 1,
+         "title": "Falling down to Earth",
+         "text": "Don't miss this rare chance to hear world-famous Laura Thompson share the secrets of her success.",
+         "categoryId": "17654",
+         "startDate": "2018-01-01 09:00:00",
+         "endDate": "2018-01-01 17:00:00",
+         "media": [  
+            {  
+               "id": 2,
+               "src": "https://www.chronoflotimeline.com/assets/images/home-events/image31.jpg",
+               "type": "Image"
+            }
+         ]
+      },
+      {  
+         "id": 3,
+         "title": "Married to Work",
+         "text": "Join us in celebrating the work of legendary artist Roberto Bertrand.",
+         "categoryId": "17653",
+         "startDate": "2018-01-07 09:00:00",
+         "endDate": "2018-01-07 17:00:00",
+         "media": [  
+            {  
+               "id": 4,
+               "src": "https://www.chronoflotimeline.com/assets/images/home-events/image30.jpg",
+               "type": "Image"
+            }
+         ]
+      }
+   ]
+)
