9b69cf288ede48f348ab3bf98c5d8cd6ccb80e5e
lemoner-rajesh <32484035+lemoner-rajesh@users.noreply.github.com>
2026-05-03 07:04:29 +0000

    adsf

--- files/003.js
+++ files/003.js
@@ -1,4 +1,4 @@
-{
+CDRonJSONPLoad({
   "categories": [
     { "id": "1", "title": "Art & Culture", "color": "#1f77b4" },
     { "id": "2", "title": "Music", "color": "#ff7f0e" },
@@ -9,212 +9,122 @@
     {
       "id": 1,
       "title": "Falling down to Earth",
-      "text": "World-famous Laura Thompson shares her journey.",
+      "text": "Laura Thompson shares her success story.",
       "fullText": "Full exhibition details...",
       "categoryId": "4",
       "dateFormat": "auto",
       "externalLink": "",
-      "media": [
-        {
-          "id": 2,
-          "src": "https://www.chronoflotimeline.com/assets/images/home-events/image31.jpg",
-          "type": "Image",
-          "imagePosition": "0,0",
-          "orderIndex": 5,
-          "eventId": 1
-        }
-      ],
+      "media": [],
       "startDate": "2018-01-01 09:00:00",
       "endDate": "2018-01-01 17:00:00"
     },
     {
-      "id": 3,
+      "id": 2,
       "title": "Married to Work",
-      "text": "Artist Roberto Bertrand exhibition.",
+      "text": "Roberto Bertrand exhibition.",
       "fullText": "Full exhibition details...",
       "categoryId": "1",
       "dateFormat": "auto",
       "externalLink": "",
-      "media": [
-        {
-          "id": 4,
-          "src": "https://www.chronoflotimeline.com/assets/images/home-events/image30.jpg",
-          "type": "Image",
-          "imagePosition": "0,0",
-          "orderIndex": 5,
-          "eventId": 3
-        }
-      ],
+      "media": [],
       "startDate": "2018-01-07 09:00:00",
       "endDate": "2018-01-07 17:00:00"
     },
     {
-      "id": 5,
-      "title": "The Stories of Pandomonium",
-      "text": "Graphic designer Gianna Rossi story.",
+      "id": 3,
+      "title": "Stories of Pandomonium",
+      "text": "Gianna Rossi design journey.",
       "fullText": "Full exhibition details...",
       "categoryId": "1",
       "dateFormat": "auto",
       "externalLink": "",
-      "media": [
-        {
-          "id": 6,
-          "src": "https://www.chronoflotimeline.com/assets/images/home-events/image12.jpg",
-          "type": "Image",
-          "imagePosition": "0,0",
-          "orderIndex": 5,
-          "eventId": 5
-        }
-      ],
+      "media": [],
       "startDate": "2018-01-11 09:00:00",
       "endDate": "2018-01-11 17:00:00"
     },
     {
-      "id": 7,
+      "id": 4,
       "title": "Time waits for No 1",
-      "text": "Musician Paul Williams exhibition.",
+      "text": "Paul Williams music showcase.",
       "fullText": "Full exhibition details...",
       "categoryId": "2",
       "dateFormat": "auto",
       "externalLink": "",
-      "media": [
-        {
-          "id": 8,
-          "src": "https://www.chronoflotimeline.com/assets/images/home-events/image14.jpg",
-          "type": "Image",
-          "imagePosition": "0,0",
-          "orderIndex": 5,
-          "eventId": 7
-        }
-      ],
+      "media": [],
       "startDate": "2018-01-12 09:00:00",
       "endDate": "2018-01-12 17:00:00"
     },
     {
-      "id": 9,
+      "id": 5,
       "title": "Autodidact Automator",
-      "text": "Mathematician Paul Laurent story.",
+      "text": "Paul Laurent mathematics story.",
       "fullText": "Full exhibition details...",
       "categoryId": "3",
       "dateFormat": "auto",
       "externalLink": "",
-      "media": [
-        {
-          "id": 10,
-          "src": "https://www.chronoflotimeline.com/assets/images/home-events/image27.jpg",
-          "type": "Image",
-          "imagePosition": "0,0",
-          "orderIndex": 5,
-          "eventId": 9
-        }
-      ],
+      "media": [],
       "startDate": "2018-01-13 09:00:00",
       "endDate": "2018-01-13 17:00:00"
     },
     {
-      "id": 11,
-      "title": "Me, you and I. Why?",
-      "text": "Musician Anna Lambert event.",
+      "id": 6,
+      "title": "Me, You and I",
+      "text": "Anna Lambert performance.",
       "fullText": "Full exhibition details...",
       "categoryId": "2",
       "dateFormat": "auto",
       "externalLink": "",
-      "media": [
-        {
-          "id": 12,
-          "src": "https://www.chronoflotimeline.com/assets/images/home-events/image30.jpg",
-          "type": "Image",
-          "imagePosition": "0,0",
-          "orderIndex": 5,
-          "eventId": 11
-        }
-      ],
+      "media": [],
       "startDate": "2018-01-14 09:00:00",
       "endDate": "2018-01-14 17:00:00"
     },
     {
-      "id": 13,
+      "id": 7,
       "title": "Great Barrier Grief",
-      "text": "Mathematician Gianna Moreau story.",
+      "text": "Gianna Moreau mathematics journey.",
       "fullText": "Full exhibition details...",
       "categoryId": "3",
       "dateFormat": "auto",
       "externalLink": "",
-      "media": [
-        {
-          "id": 14,
-          "src": "https://www.chronoflotimeline.com/assets/images/home-events/image31.jpg",
-          "type": "Image",
-          "imagePosition": "0,0",
-          "orderIndex": 5,
-          "eventId": 13
-        }
-      ],
-      "startDate": "2018-01-14 09:00:00",
-      "endDate": "2018-01-14 17:00:00"
+      "media": [],
+      "startDate": "2018-01-15 09:00:00",
+      "endDate": "2018-01-15 17:00:00"
     },
     {
-      "id": 15,
+      "id": 8,
       "title": "Chained to Destiny",
-      "text": "Philosopher Amelia Bonnet exhibition.",
+      "text": "Amelia Bonnet philosophy exhibition.",
       "fullText": "Full exhibition details...",
       "categoryId": "4",
       "dateFormat": "auto",
       "externalLink": "",
-      "media": [
-        {
-          "id": 16,
-          "src": "https://www.chronoflotimeline.com/assets/images/home-events/image23.jpg",
-          "type": "Image",
-          "imagePosition": "0,0",
-          "orderIndex": 5,
-          "eventId": 15
-        }
-      ],
+      "media": [],
       "startDate": "2018-01-17 09:00:00",
       "endDate": "2018-01-17 17:00:00"
     },
     {
-      "id": 17,
+      "id": 9,
       "title": "Jokes in Jeopardy",
-      "text": "Artist Giuseppe Roux talk.",
+      "text": "Giuseppe Roux art showcase.",
       "fullText": "Full exhibition details...",
       "categoryId": "1",
       "dateFormat": "auto",
       "externalLink": "",
-      "media": [
-        {
-          "id": 18,
-          "src": "https://www.chronoflotimeline.com/assets/images/home-events/image36.jpg",
-          "type": "Image",
-          "imagePosition": "0,0",
-          "orderIndex": 5,
-          "eventId": 17
-        }
-      ],
-      "startDate": "2018-01-17 09:00:00",
-      "endDate": "2018-01-17 17:00:00"
+      "media": [],
+      "startDate": "2018-01-18 09:00:00",
+      "endDate": "2018-01-18 17:00:00"
     },
     {
-      "id": 19,
+      "id": 10,
       "title": "Lighting up the Past",
-      "text": "Dancer Victor Evans performance.",
+      "text": "Victor Evans dance performance.",
       "fullText": "Full exhibition details...",
       "categoryId": "2",
       "dateFormat": "auto",
       "externalLink": "",
-      "media": [
-        {
-          "id": 20,
-          "src": "https://www.chronoflotimeline.com/assets/images/home-events/image25.jpg",
-          "type": "Image",
-          "imagePosition": "0,0",
-          "orderIndex": 5,
-          "eventId": 19
-        }
-      ],
-      "startDate": "2018-01-18 09:00:00",
-      "endDate": "2018-01-18 17:00:00"
+      "media": [],
+      "startDate": "2018-01-19 09:00:00",
+      "endDate": "2018-01-19 17:00:00"
     }
   ]
-}
+});
