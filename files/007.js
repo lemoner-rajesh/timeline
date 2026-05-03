dea7cec9309a345ade58d23bd954b4c64b323c49
lemoner-rajesh <32484035+lemoner-rajesh@users.noreply.github.com>
2026-05-03 07:31:38 +0000

    asdf

---
+++ files/007.js
@@ -0,0 +1,50 @@
+CDRonJSONPLoad([
+  {
+    "id": 1,
+    "title": "Art Exhibition",
+    "text": "Art event",
+    "fullText": "Details",
+    "categoryId": 17649,
+    "dateFormat": "auto",
+    "externalLink": "",
+    "media": [],
+    "startDate": "2020-01-01 00:00:00",
+    "endDate": "2020-01-01 00:00:00"
+  },
+  {
+    "id": 2,
+    "title": "Music Concert",
+    "text": "Music event",
+    "fullText": "Details",
+    "categoryId": 17650,
+    "dateFormat": "auto",
+    "externalLink": "",
+    "media": [],
+    "startDate": "2020-01-05 00:00:00",
+    "endDate": "2020-01-05 00:00:00"
+  },
+  {
+    "id": 3,
+    "title": "Science Fair",
+    "text": "Science event",
+    "fullText": "Details",
+    "categoryId": 17652,
+    "dateFormat": "auto",
+    "externalLink": "",
+    "media": [],
+    "startDate": "2020-01-10 00:00:00",
+    "endDate": "2020-01-10 00:00:00"
+  },
+  {
+    "id": 4,
+    "title": "Philosophy Talk",
+    "text": "Philosophy event",
+    "fullText": "Details",
+    "categoryId": 17651,
+    "dateFormat": "auto",
+    "externalLink": "",
+    "media": [],
+    "startDate": "2020-01-12 00:00:00",
+    "endDate": "2020-01-12 00:00:00"
+  }
+])
