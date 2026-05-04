246813eed915862c351fe66679e7aec946233f43
lemoner-rajesh <32484035+lemoner-rajesh@users.noreply.github.com>
2026-05-04 12:24:51 +0000

    asdf

---
+++ files/05.js
@@ -0,0 +1,52 @@
+CDRonJSONPLoad([
+  {
+    "id": 1,
+    "title": "Event One",
+    "text": "Category: abc | Section: Phase 1",
+    "categoryId": "abc",
+    "sectionId": "Phase 1",
+    "startDate": "2018-01-01 09:00:00",
+    "endDate": "2018-01-01 17:00:00",
+    "media": [{"id": 10, "src": "https://www.chronoflotimeline.com/assets/images/home-events/image31.jpg", "type": "Image"}]
+  },
+  {
+    "id": 2,
+    "title": "Event Two",
+    "text": "Category: xyz | Section: Phase 1",
+    "categoryId": "xyz",
+    "sectionId": "Phase 1",
+    "startDate": "2018-01-05 09:00:00",
+    "endDate": "2018-01-05 17:00:00",
+    "media": [{"id": 11, "src": "https://www.chronoflotimeline.com/assets/images/home-events/image30.jpg", "type": "Image"}]
+  },
+  {
+    "id": 3,
+    "title": "Event Three",
+    "text": "Category: pqr | Section: Phase 2",
+    "categoryId": "pqr",
+    "sectionId": "Phase 2",
+    "startDate": "2018-01-10 09:00:00",
+    "endDate": "2018-01-10 17:00:00",
+    "media": [{"id": 12, "src": "https://www.chronoflotimeline.com/assets/images/home-events/image12.jpg", "type": "Image"}]
+  },
+  {
+    "id": 4,
+    "title": "Event Four",
+    "text": "Category: abc | Section: Phase 2",
+    "categoryId": "abc",
+    "sectionId": "Phase 2",
+    "startDate": "2018-01-15 09:00:00",
+    "endDate": "2018-01-15 17:00:00",
+    "media": [{"id": 13, "src": "https://www.chronoflotimeline.com/assets/images/home-events/image14.jpg", "type": "Image"}]
+  },
+  {
+    "id": 5,
+    "title": "Event Five",
+    "text": "Category: xyz | Section: Phase 1",
+    "categoryId": "xyz",
+    "sectionId": "Phase 1",
+    "startDate": "2018-01-20 09:00:00",
+    "endDate": "2018-01-20 17:00:00",
+    "media": [{"id": 14, "src": "https://www.chronoflotimeline.com/assets/images/home-events/image27.jpg", "type": "Image"}]
+  }
+])
