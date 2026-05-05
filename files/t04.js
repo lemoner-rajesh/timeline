f01117cbf521ea6c87e35243b93610c44481236e
lemoner-rajesh <32484035+lemoner-rajesh@users.noreply.github.com>
2026-05-05 13:07:09 +0000

    asdf

---
+++ files/t04.js
@@ -0,0 +1,46 @@
+CDRonJSONPLoad([
+  {
+    "id": 1,
+    "title": "Test: Using tagId",
+    "text": "This event uses the singular 'tagId' key.",
+    "categoryId": "Business",
+    "sectionId": "Phase 1",
+    "tagId": "1980s", 
+    "startDate": "1982-03-10 09:00:00",
+    "endDate": "1982-03-10 17:00:00",
+    "media": [{"id": 301, "src": "https://www.chronoflotimeline.com/assets/images/home-events/image31.jpg", "type": "Image"}]
+  },
+  {
+    "id": 2,
+    "title": "Test: Using tagsId",
+    "text": "This event uses the plural 'tagsId' key.",
+    "categoryId": "Family",
+    "sectionId": "Phase 2",
+    "tagsId": "1980s",
+    "startDate": "1985-08-22 10:00:00",
+    "endDate": "1985-08-22 18:00:00",
+    "media": [{"id": 302, "src": "https://www.chronoflotimeline.com/assets/images/home-events/image30.jpg", "type": "Image"}]
+  },
+  {
+    "id": 3,
+    "title": "Test: Multiple Tags in tagId",
+    "text": "Testing if comma-separated names work in tagId.",
+    "categoryId": "Philanthropy",
+    "sectionId": "Phase 1",
+    "tagId": "1990s, Milestone",
+    "startDate": "1994-11-05 09:00:00",
+    "endDate": "1994-11-05 17:00:00",
+    "media": [{"id": 303, "src": "https://www.chronoflotimeline.com/assets/images/home-events/image12.jpg", "type": "Image"}]
+  },
+  {
+    "id": 4,
+    "title": "Test: Multiple Tags in tagsId",
+    "text": "Testing if comma-separated names work in tagsId.",
+    "categoryId": "World Events",
+    "sectionId": "Phase 2",
+    "tagsId": "1990s, Global",
+    "startDate": "1999-12-31 23:00:00",
+    "endDate": "2000-01-01 05:00:00",
+    "media": [{"id": 304, "src": "https://www.chronoflotimeline.com/assets/images/home-events/image14.jpg", "type": "Image"}]
+  }
+])
